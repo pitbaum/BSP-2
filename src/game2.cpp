b18010d94e669dd68108cdd9374de487fe15cc40
pitbaum <pit.baum.001@student.uni.lu>
2021-04-12 13:55:48 +0200

    Week 5 commit

--- src/game2.cpp
+++ src/game2.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <fstream>
+#include <ctime>
 using namespace std;
+
 
 class GameGrid {
   private:
@@ -151,6 +154,7 @@ class GameGrid {
 };
 
 int main() {
+  srand(time(0));
   GameGrid game1;
   int currentRound = -1;
   char currentPlayer;
@@ -170,22 +174,37 @@ int main() {
   return 0;
 }
 
+//Agent that uses reinforcement learning.
 class Agent1 {
   private:
-    int gridSize = 9; // the size of the play field.
-    char grid[9];
-    State listOfCurrentStates[9];
+    string currentGameState;  //string on the current game state.
+    State listOfCurrentStates[9]; //list of moves taken in the game.
     int reward; //if win = 1, if loose = -1, if draw = 0.
     State listOfStates[255168]; //look up table.
-    int explorationRate = 25 //percentage of the moves used to explore. (max 100 min 0)
+    int explorationRate = 75; //percentage of the moves used to explore. (max 100 min 0)
+    bool isPlayer1;  //true if agent is player 1, false if agent is player 2.
 
   public:
-    Agent1()
+    Agent1(bool pIsPlayer1)
     {
-      //for (int i = 0; i < 255168; ++i)
-       //{
-         //listOfStates[i] = fileRead;
-       //}
+      //reads from file the knowledge representation.
+      //initializes at the start of the program the listOfStates.
+      string readText;
+      ifstream readFile("agent3x3.txt");
+      int stateIndexer = 0;
+      int currentPosition = 0;
+      int currentValue = 0;
+      while (getline (readFile, readText)) {
+        if(stateIndexer%2 == 0)
+          currentPosition = (int) readText;
+        else {
+          currentValue = (int) readText;
+          listOfStates[(stateIndexer - 1)/2] = new State(currentPosition, currentValue);
+        }
+        stateIndexer++;
+      }
+      readFile.close();
+      isPlayer1 = pIsPlayer1;
     }
 
     int makeMove() //returns the chosen move.
@@ -193,12 +212,86 @@ class Agent1 {
 
     }
 
-    void Policy()//chooses the move to execute.
+    string Policy()//chooses and returns the move to execute.
     {
+      //creates an array possibleMoves[].
+      //Which consist of all the moves possible to make at the current game state.
+      int moveCounter = 0;
+      for(char& c : currentGameState) {
+        if(c == " ") {
+          moveCounter++;
+        }
+      }
+      string possibleMoves[moveCounter];
+      for(int i = 0; i < currentGameState.size(); i++)
+      {
+        if(currentGameState[i] == " ")
+        {
+          string move = currentGameState;
+          if(isPlayer1)
+            move[i] = "X";
+          else
+            move[i] = "O";
+          possibleMoves[moveCounter] = move;
+          moveCounter--;
+        }
+      }
 
+      //finds the possible moves in the look up table and searches for the state with the highest value.
+      //if a possible state is not yet in the table, the state will be added.
+      int highestIndex;
+      int highestValue = -2;
+      for(int j = 0; j < moveCounter; j++)
+      {
+        bool foundInTable = false;
+       for(int listOfStatesIterator = 0; listOfStatesIterator < listOfStates.size(); listOfStatesIterator++)
+       {
+          if(possibleMoves[j] == listOfStates[listOfStatesIterator].positions)
+          {
+            foundInTable = true;
+            if(listOfStates[listOfStatesIterator].reward > highestValue)
+              highestIndex = listOfStatesIterator;
+          }
+          if(foundInTable)
+            break;
+          if(listOfStatesIterator == listOfStates.size()-1)
+          {
+            listOfStates[listOfStatesIterator] = new State(possibleMoves[j], 0);
+          }
+        }
+      }
+
+      //If the random exploration rate is triggered.
+      //Choose a random move.
+      //Else return best valued move.
+      if((rand() % 100) > explorationRate)
+        return possibleMoves(rand() % moveCounter)
+      else
+        return listOfStates[highestIndex].positions;
     }
 
-    void rewardUpdate(int pEndGameStatus) //distibutes the reward link to the different states to the look up table.
+    string gridConverterTo1D(char pGrid[3][3]) //converts the entered 2D into 1D string.
+    {
+      char position[9];
+      int currentPosition = 0;
+      for (int i = 0; i < 3; ++i)
+      {
+        for (int j = 0; j < 3; ++j)
+        {
+          position[currentPosition] = pGrid[i][j];
+          currentPosition++;
+        }
+      }
+      string grid;
+      for (int i = 0; i < sizeof(position); ++i)
+      {
+        grid += position[i];
+      }
+      return grid;
+    }
+
+    //distibutes the reward link to the different states to the look up table.
+    void rewardUpdate(int pEndGameStatus)
     {
       for (int i = 0; i < listOfCurrentStates.size(); --i)
       {
@@ -216,25 +309,15 @@ class Agent1 {
         }
       }
     }
-
-    void updateGrid(char pGrid[3][3]) //updates the game grid of the player and the actual game.
-    {
-      int index = 0;
-      for (int i = 0; i < 9; ++i)
-      {
-        for (int j = 0; j < count; ++j)
-       {
-          grid[index] = pGrid[i][j];
-          index++;
-        }
-      }
-
-    }
 };
 
+//knowledge representation of the Agent1
 class State() {
   public:
-    int positions;
+    //number code that tells you the state of every Field.
+    //" " = empty field, X field of status player 1, O (letter) field of status player 2.
+    string positions;
+    //relative reward value distributed to the field.
     int reward = 0;
 
     State(int pPositions, int pReward)
